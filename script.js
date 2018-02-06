0e8a89cee325ace2e23baebc1904affc478134e9
Tamara Nechyporenko <tamaranechyporenko@gmail.com>
2018-02-06 20:52:57 +0200

    Commiting

---
+++ script.js
@@ -0,0 +1,52 @@
+var stickyHeader, sticky;
+window.onload=function () {
+    checkOrientation();
+};
+
+function checkOrientation()
+{
+    if(window.innerWidth<=800)
+    {
+        stickyHeader = document.getElementById("mobileNav");
+    }
+    else
+    {
+        stickyHeader = document.getElementById("desktopNav");
+    }
+    sticky = stickyHeader.offsetTop;
+};
+
+window.onresize = function(){
+    checkOrientation();
+};
+window.onscroll=function () {
+    if (window.pageYOffset > sticky)
+    {
+        stickyHeader.classList.add("sticky-header");
+    }
+    else
+    {
+        stickyHeader.classList.remove("sticky-header");
+    }
+};
+
+function showHideMenu() {
+    var burgerMenu=document.getElementById("burger-menu");
+    var mobileUl=document.getElementById("mobile-ul");
+    if(hasClass(burgerMenu, "burger-menu"))
+    {
+        burgerMenu.classList.remove("burger-menu");
+        burgerMenu.classList.add("close-menu");
+        mobileUl.classList.remove("display-none");
+    }
+    else if(hasClass(burgerMenu, "close-menu"))
+    {
+        burgerMenu.classList.add("burger-menu");
+        burgerMenu.classList.remove("close-menu");
+        mobileUl.classList.add("display-none");
+    }
+}
+
+function hasClass(element, cls) {
+    return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
+}
